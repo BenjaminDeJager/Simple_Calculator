f44ec064b67bed44743d047754459df56efa0923
BenjaminDeJager <61642215+BenjaminDeJager@users.noreply.github.com>
2022-01-14 19:06:24 -0800

    Completed step 6 and added a "clear" button, note changes in readme

--- README.md
+++ README.md
@@ -5,42 +5,49 @@
    _(I already have a template for a window with *a* button)._
    2. Create github repo and push. 
    (explicitly adding this item as I forget to do this alot and end up with a commit-mess after too much stuff happens).
-
-DONE
+   
+**DONE**
 2. Setting up buttons
 
    1. Make a input-button "class" that displays an initialization string as a parameter 
-   so I can create 0-9 and "." button's generically. 
+   so I can create 0-9 and "." button's generically.
 
-DONE
-   <br>_(this will functionality be identical to Kivy's Button class at this point and won't do anything)_</p>
+**DONE**
+
+   (this will functionality be identical to Kivy's Button class at this point and won't do anything)
    2. Give the input-buttons a generic "press" function that simply displays the string 
-to confirm the buttons work. (kivy should have a "click" function irrc)
+to confirm the buttons work. 
+   <br>(kivy should have a "click" function irrc)<br>
 
-DONE
+**DONE**
 
 3. Setting up input memory and display
    1. Add a "Memory" list to the window that the buttons can "push" to as a queue that flushes the oldest object when full.</p>
    2. Add a "Display" Text object as a field of the "memory" object. Display each element in ascending order (akin to Texas instrument calculators).
 
-DONE, Memory is a class that extends Deque's and holds str objects that get passed by the buttons when pressed.
-Label gets passed in from CalculatorApp (changed later).
+**DONE, Memory is a class that extends Dequeues and holds str objects that get passed by the buttons when pressed.
+Label gets past in from CalculatorApp (changed later).**
 
 4. adding some type of computational function
    1. Add a "+" button such that it pushes the sum of the newest 2 elements in memory, to the memory queue.</p>
 
 Now meets minimum requirements of a virtual calculator (by my definition).
 
-DONE, changed Memory to initalize it's label inside itself. 
-removed "." button for now as it cannot be converted by adderbutton
+**DONE, changed Memory to initialize it's label inside itself. 
+removed "." button for now as it cannot be converted by the adder button**
 
 5. extending functionality to something approaching useful
     1. add a "current_value" field to a "Input" text item beneath Display, as a empty string by default, 
 that the buttons push to by appending their value to it instead of Display and then make "=" push this to memory.
 
+**NA: decided to not re-factor to a non-"deck" based calculator (for now).**
+
 Now can input values larger than 9.
 
 6. bonus functionality
-    1. Try to expand "+" to a "operator" button class which pulls from memory, then pushs to memory the results of applying a initalized lambda function on the memory nondestructively.
+    1. Try to expand "+" to an "operator" button class which pulls from memory, then pushes to memory the results of applying an initialized lambda function on the memory nondestructively.
     2. Use this to create subtraction, multiplication and integer division.
     3. Add input validation.
+    
+**DONE, +, -, \*, / (floating division), and "CLEAR" added. Pushed to repo and made public.**
+
--- main.py
+++ main.py
@@ -6,49 +6,50 @@ from kivy.uix.label import Label
 from collections import deque
 from kivy.uix.widget import Widget
 
+
 class Memory(deque):
     def __init__(self, maxSize=5):
         super().__init__(maxlen=maxSize)
-        self.label = Label()
+        self.label = Label(font_size='30sp')
         self.maxSize = maxSize
 
     def __str__(self):
         return "\n".join(self)
 
 
-class AddButton(Button):
-    def __init__(self, theMemory, **kwargs):
+class MemoryButton(Button):
+    def __init__(self, newName, theMemory, **kwargs):
         super().__init__(**kwargs)
-        self.text = "+"
         self.theMemory = theMemory
+        self.text = newName
+
+
+class OperatorButton(MemoryButton):
+    def __init__(self, theName, theMemory, theOperation, **kwargs):
+        super().__init__(theName, theMemory, **kwargs)
+        self.operation = theOperation
 
     def on_press(self):
         try:
-            self.theMemory.append(str(float(self.theMemory[-1]) + float(self.theMemory[-2])))
+            newValue = self.operation(self, self.theMemory)
+            if newValue is not None:
+                self.theMemory.append(str(newValue))
             self.theMemory.label.text = str(self.theMemory)
         except IndexError:
-            print("cannot add, not enough inputs")
+            print("IndexError")
         except TypeError:
-            print("values cannot both be converted")
+            print("TypeError")
 
-
-class ValueButton(Button):
-    def __init__(self, text, theMemory, **kwargs):
-        super().__init__(**kwargs)
-        self.text = text
-        self.theMemory = theMemory
-        size_hint = (1, 1)
-
-    def on_press(self):
-        self.theMemory.append(self.text)
-        self.theMemory.label.text = str(self.theMemory)
 
 class CalculatorApp(App):
-
     def __init__(self):
         super(CalculatorApp, self).__init__()
         self.memory = Memory()
-        self.display = self.memory.label
+        self.display = BoxLayout(orientation='horizontal')
+        self.display.add_widget(self.memory.label)
+        self.display.add_widget(
+            OperatorButton("CLEAR", theMemory=self.memory,
+                           theOperation=lambda _self, _memory: _memory.clear(), size_hint=(0.25, 0.5)))
 
     def build(self):
         main = BoxLayout(orientation='vertical', size=(50, 30))
@@ -63,16 +64,39 @@ class CalculatorApp(App):
         for i in range(numRows):
             row = BoxLayout(orientation='horizontal')
             for j in range(numCols):
-                button = ValueButton(str(counter), theMemory=self.memory)
+                string_counter = str(counter)
+                button = OperatorButton(str(counter), theMemory=self.memory,
+                                        theOperation=lambda _self, _memory: _self.text)
                 buttons.append(button)
                 row.add_widget(button)
                 counter = counter - 1
             buttonBox.add_widget(row)
 
         row_4 = BoxLayout(orientation='horizontal')
-        row_4.add_widget(ValueButton('0', theMemory=self.memory))
-        row_4.add_widget(AddButton(theMemory=self.memory))
+        row_4.add_widget(OperatorButton("0", theMemory=self.memory,
+                                        theOperation=lambda _self, _memory: _self.text))
+        row_4.add_widget(Button(disabled=True, size_hint=(2, 1)))
         buttonBox.add_widget(row_4)
+
+        row_operators = BoxLayout(orientation='horizontal')
+
+        row_operators.add_widget(OperatorButton("+", theMemory=self.memory,
+                                                theOperation=lambda _self, _memory: float(_memory[-1]) + float(
+                                                    _memory[-2])
+                                                ))
+        row_operators.add_widget(OperatorButton("-", theMemory=self.memory,
+                                                theOperation=lambda _self, _memory: float(_memory[-1]) - float(
+                                                    _memory[-2])
+                                                ))
+        row_operators.add_widget(OperatorButton("*", theMemory=self.memory,
+                                                theOperation=lambda _self, _memory: float(_memory[-1]) * float(
+                                                    _memory[-2])
+                                                ))
+        row_operators.add_widget(OperatorButton("/", theMemory=self.memory,
+                                                theOperation=lambda _self, _memory: float(_memory[-1]) / float(
+                                                    _memory[-2])
+                                                ))
+        buttonBox.add_widget(row_operators)
 
         main.add_widget(buttonBox)
 
