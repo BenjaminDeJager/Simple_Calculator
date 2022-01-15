0b8ec7d7640bf5dc9e10046262a39919e8166365
BenjaminDeJager <61642215+BenjaminDeJager@users.noreply.github.com>
2022-01-14 17:53:13 -0800

    Completed steps 2 through 4, see updated README for details. pushing to repo.

--- README.md
+++ README.md
@@ -6,22 +6,33 @@
    2. Create github repo and push. 
    (explicitly adding this item as I forget to do this alot and end up with a commit-mess after too much stuff happens).
 
+DONE
 2. Setting up buttons
 
    1. Make a input-button "class" that displays an initialization string as a parameter 
    so I can create 0-9 and "." button's generically. 
+
+DONE
    <br>_(this will functionality be identical to Kivy's Button class at this point and won't do anything)_</p>
    2. Give the input-buttons a generic "press" function that simply displays the string 
 to confirm the buttons work. (kivy should have a "click" function irrc)
+
+DONE
 
 3. Setting up input memory and display
    1. Add a "Memory" list to the window that the buttons can "push" to as a queue that flushes the oldest object when full.</p>
    2. Add a "Display" Text object as a field of the "memory" object. Display each element in ascending order (akin to Texas instrument calculators).
 
+DONE, Memory is a class that extends Deque's and holds str objects that get passed by the buttons when pressed.
+Label gets passed in from CalculatorApp (changed later).
+
 4. adding some type of computational function
    1. Add a "+" button such that it pushes the sum of the newest 2 elements in memory, to the memory queue.</p>
 
 Now meets minimum requirements of a virtual calculator (by my definition).
+
+DONE, changed Memory to initalize it's label inside itself. 
+removed "." button for now as it cannot be converted by adderbutton
 
 5. extending functionality to something approaching useful
     1. add a "current_value" field to a "Input" text item beneath Display, as a empty string by default, 
--- main.py
+++ main.py
@@ -2,29 +2,80 @@ from kivy.app import App
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from collections import deque
 from kivy.uix.widget import Widget
 
+class Memory(deque):
+    def __init__(self, maxSize=5):
+        super().__init__(maxlen=maxSize)
+        self.label = Label()
+        self.maxSize = maxSize
 
-class valueButton(Button):
-    def __init__(self, text, **kwargs):
+    def __str__(self):
+        return "\n".join(self)
+
+
+class AddButton(Button):
+    def __init__(self, theMemory, **kwargs):
+        super().__init__(**kwargs)
+        self.text = "+"
+        self.theMemory = theMemory
+
+    def on_press(self):
+        try:
+            self.theMemory.append(str(float(self.theMemory[-1]) + float(self.theMemory[-2])))
+            self.theMemory.label.text = str(self.theMemory)
+        except IndexError:
+            print("cannot add, not enough inputs")
+        except TypeError:
+            print("values cannot both be converted")
+
+
+class ValueButton(Button):
+    def __init__(self, text, theMemory, **kwargs):
         super().__init__(**kwargs)
         self.text = text
-        size_hint = (.7, 1)
+        self.theMemory = theMemory
+        size_hint = (1, 1)
 
-    def on_release(self):
-        print(self.text)
-
+    def on_press(self):
+        self.theMemory.append(self.text)
+        self.theMemory.label.text = str(self.theMemory)
 
 class CalculatorApp(App):
 
-    def build(self):
-        buttons = []
-        for i in range(10):
-            buttons.append(valueButton(str(i)))
-        main = BoxLayout(orientation='horizontal')
+    def __init__(self):
+        super(CalculatorApp, self).__init__()
+        self.memory = Memory()
+        self.display = self.memory.label
 
-        for button in buttons:
-            main.add_widget(button)
+    def build(self):
+        main = BoxLayout(orientation='vertical', size=(50, 30))
+
+        main.add_widget(self.display)
+
+        buttons = []
+        numRows = 3
+        numCols = 3
+        counter = 9
+        buttonBox = BoxLayout(orientation='vertical')
+        for i in range(numRows):
+            row = BoxLayout(orientation='horizontal')
+            for j in range(numCols):
+                button = ValueButton(str(counter), theMemory=self.memory)
+                buttons.append(button)
+                row.add_widget(button)
+                counter = counter - 1
+            buttonBox.add_widget(row)
+
+        row_4 = BoxLayout(orientation='horizontal')
+        row_4.add_widget(ValueButton('0', theMemory=self.memory))
+        row_4.add_widget(AddButton(theMemory=self.memory))
+        buttonBox.add_widget(row_4)
+
+        main.add_widget(buttonBox)
+
         return main
 
 
